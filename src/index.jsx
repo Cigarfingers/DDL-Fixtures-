2a50f3d8927694c7f4403938399c3d73db533a1a
Cigarfingers <Discorddartsleagues@gmail.com>
2025-10-17 22:25:48 +0100

    Replace App component with DartsFixtureManager

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import DartsFixtureManager from './DartsFixtureManager'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <DartsFixtureManager />
+  </React.StrictMode>,
 )
-```
-
-### **6. Create `src/App.jsx`:**
-Copy your updated React component code (the one without the leg badges) into this file.
-
-### **7. Create `.gitignore`:**
-```
-node_modules
-dist
-.DS_Store
