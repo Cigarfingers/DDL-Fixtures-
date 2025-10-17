246dbd8e1b4ab118b421179a4078c4e485942ab9
Cigarfingers <Discorddartsleagues@gmail.com>
2025-10-17 22:05:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DDL-Fixtures/'
+  base: '/DDL-Fixtures-/',   // ← must match the repo name exactly
 })
